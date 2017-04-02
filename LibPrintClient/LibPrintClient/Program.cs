0cdda88fb40c3367d649fb5d9857100979936f83
Robert <morpheus9and20@gmail.com>
2017-04-02 18:53:01 -0500

    General updates to LibPrint Client

--- LibPrintClient/LibPrintClient/Program.cs
+++ LibPrintClient/LibPrintClient/Program.cs
@@ -20,26 +20,36 @@ namespace LibPrintClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            string result;
+            string error;
 
             //check contents of cache folder
             if (Directory.GetFiles(@"c:\ProgramData\LibPrint\cache\").Length == 0)
             {
-                string error = "Response: Error\nError: No document cached for printing";
+                error = "Response: Error\nError: No document cached for printing";
                 Variables.parsed = error.Split(new[] {':', '\n'});
 
                 Application.Run(new PrintError());
             }
 
-            Console.WriteLine(Variables.username);
+            try
+            {
+                //Make getInformation request
+                WebClient webClient = new WebClient();
+                webClient.QueryString.Add("request", "getInformation");
+                webClient.QueryString.Add("username", Variables.username);
+                webClient.QueryString.Add("computer", Variables.computer);
+                webClient.QueryString.Add("secToken", Variables.GenerateSecToken(Variables.domainCode, Variables.username, Variables.computer));
+                result = webClient.DownloadString(Variables.libprinturl);
+            }
 
-            //Make getInformation request
-            WebClient webClient = new WebClient();
-            webClient.QueryString.Add("request", "getInformation");
-            webClient.QueryString.Add("username", Variables.username);
-            webClient.QueryString.Add("computer", Variables.computer);
-            webClient.QueryString.Add("secToken", Variables.GenerateSecToken("temp", Variables.username, Variables.computer));
-            string result = webClient.DownloadString(Variables.libprinturl);
-            Variables.parsed = result.Split(new[] {':', '\n'});
+            catch(WebException ex)
+            {
+                //Handle server response exception
+                result = string.Format("Response: Error\nError: Error connecting to server {0}", ex);
+            }
+
+            Variables.parsed = result.Split(new[] { ':', '\n' });
 
             if (Variables.parsed[1].Trim() == "OK")
             {
--- LibPrintClient/LibPrintClient/Variables.cs
+++ LibPrintClient/LibPrintClient/Variables.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace LibPrintClient
 {
-    class Variables
+    public static class Variables
     {
         public static string username = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
         public static string computer = Environment.MachineName;
@@ -16,6 +16,7 @@ namespace LibPrintClient
         public static string libprinturl = "http://chandler.io/LibPrint/RequestHandler";
         public static string printerName;
         public static string cacheFile = Directory.GetFiles(@"c:\ProgramData\LibPrint\cache\")[0];
+        public static string domainCode = System.IO.File.ReadAllText(@"C:\Users\Robert\Source\Repos\LibPrint-Client\LibPrintClient\LibPrintClient\DomainCodeConfig.ini").Split('=')[1].Trim();
 
         public static string GenerateSecToken(string domainCode, string username, string computer)
         {
